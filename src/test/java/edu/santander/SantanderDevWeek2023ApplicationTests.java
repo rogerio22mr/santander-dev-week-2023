813bcfe4844000d7b14a0effd090b796c213db00
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-10-09 19:31:54 -0300

    Initial commit

---
+++ src/test/java/edu/santander/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
