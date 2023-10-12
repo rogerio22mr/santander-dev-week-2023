8e4cfb2a3fd03998640590bf286443e18c5158f0
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-10-12 19:27:46 -0300

    Implementação da camada de dominio

---
+++ src/main/java/edu/santander/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.santander.domain.repository;
+
+import edu.santander.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
