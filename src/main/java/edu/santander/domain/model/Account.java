e0f3535e455068115e774fb1ed6a80aa688445aa
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-10-12 19:30:39 -0300

    Hotfix "precision" e "scale"

--- src/main/java/edu/santander/domain/model/Account.java
+++ src/main/java/edu/santander/domain/model/Account.java
@@ -13,9 +13,9 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(precision = 2, scale = 13)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "additional_limit", precision = 2, scale = 13)
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
--- src/main/java/edu/santander/domain/model/Card.java
+++ src/main/java/edu/santander/domain/model/Card.java
@@ -11,7 +11,7 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String number;
-    @Column(name = "available_limit", precision = 2, scale = 13)
+    @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
