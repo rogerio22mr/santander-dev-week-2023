4d6f5f2dd95b72ea5df5acd553c46c901e0dc5b9
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-10-14 20:24:58 -0300

    Bugfix CORS

--- src/main/java/edu/santander/Application.java
+++ src/main/java/edu/santander/Application.java
@@ -1,8 +1,11 @@
 package edu.santander;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Deafult Server URL")})
 @SpringBootApplication
 public class Application {
 
